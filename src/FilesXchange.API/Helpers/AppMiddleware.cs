b9b93d95483acd64113d82a79bf9907fcebc71e4
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2026-04-10 14:26:08 +0500

    Added AppMiddleware class

---
+++ src/FilesXchange.API/Helpers/AppMiddleware.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace FilesXchange.API.Helpers;
+
+public static class AppMiddleware
+{
+    public static void ConfigureMiddleware(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        // app.UseHttpsRedirection();
+    }
+}
--- src/FilesXchange.API/Program.cs
+++ src/FilesXchange.API/Program.cs
@@ -6,12 +6,7 @@ builder.ConfigureServices();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
+app.ConfigureMiddleware();
 
 
 app.Run();
