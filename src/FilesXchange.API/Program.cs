5c907640f7ed15de1ecc8bce4e920215ff296958
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2026-04-10 15:27:46 +0500

    Added initial Map-methods in Program.cs

--- src/FilesXchange.API/Program.cs
+++ src/FilesXchange.API/Program.cs
@@ -8,5 +8,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.ConfigureMiddleware();
 
+var apiEndpoints = app.MapGroup("/api");
+
+apiEndpoints.MapPost("/upload",
+    async (IList<IFormFile> files, CancellationToken ctk) =>
+{
+    //TODO: Upload method
+});
+
+apiEndpoints.MapGet("/download/{token}",
+    async (string token, CancellationToken ctk) =>
+{
+    //TODO: Download method
+});
+
+
 
 app.Run();
