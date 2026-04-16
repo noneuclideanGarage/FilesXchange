d87b01a7a80119feee139da435b650e809622434
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2026-04-16 15:28:03 +0500

    Refresh db-configuration in AppConfigurator

--- src/FilesXchange.API/Helpers/AppConfigurator.cs
+++ src/FilesXchange.API/Helpers/AppConfigurator.cs
@@ -11,16 +11,8 @@ public static class AppConfigurator
         var appSettingsSection = builder.Configuration.GetSection(AppOptions.SectionName);
         builder.Services.Configure<AppOptions>(appSettingsSection);
 
-        builder.Services.AddDbContext<FilesXchangeDbContext>(opts =>
-            {
-                string dbConfigurationKey = "DatabaseName";
-                var dbName = builder.Configuration[dbConfigurationKey];
-                var pathForDatabase = Environment
-                    .GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-
-                var DbPath = Path.Join(pathForDatabase, dbName);
-                opts.UseSqlite($"Data Source={DbPath}");
-            });
+        builder.Services.AddDbContext<FilesXchangeDbContext>(opts 
+            => opts.UseSqlite(builder.Configuration.GetConnectionString("Default")));
         builder.Services.AddControllers();
     }
 }
