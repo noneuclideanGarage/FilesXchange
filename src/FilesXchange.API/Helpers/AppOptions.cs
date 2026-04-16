0ddf5a7e9044a24bb28207819a1dc37f5dbcfe43
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2026-04-16 14:51:29 +0500

    Add AppOptions class

---
+++ src/FilesXchange.API/Helpers/AppOptions.cs
@@ -0,0 +1,22 @@
+using FilesXchange.API.Helpers.Constants;
+
+namespace FilesXchange.API.Helpers;
+
+public sealed class AppOptions
+{
+    public const string SectionName = "FilesXchange";
+
+    public long MaxFileSizeBytes { get; set; } = 2 * FileSize.GB;
+
+    public int MaxFilesPerUpload { get; set; } = 100;
+
+    public int MaxFileNameLength { get; set; } = 128;
+
+    public int TokenExpirationDays { get; set; } = 7;
+
+    public int CleanupIntervalSeconds { get; set; } = 300;
+
+    public string UploadDirectory { get; set; } = "uploads";
+
+    public string LogDirectory { get; set; } = "logs";
+}
