b3ab6df9bbfeb1b0e1715fbd49692451f1687db5
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2026-04-16 16:38:41 +0500

    Configure app with AppOptions

--- src/FilesXchange.API/Helpers/AppConfigurator.cs
+++ src/FilesXchange.API/Helpers/AppConfigurator.cs
@@ -1,5 +1,6 @@
 using FilesXchange.API.Data;
 using FilesXchange.API.Options;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
 
 namespace FilesXchange.API.Helpers;
@@ -7,11 +8,25 @@ namespace FilesXchange.API.Helpers;
 public static class AppConfigurator
 {
     public static void ConfigureServices(this WebApplicationBuilder builder)
-    {   
+    {
         var appSettingsSection = builder.Configuration.GetSection(AppOptions.SectionName);
         builder.Services.Configure<AppOptions>(appSettingsSection);
+        builder.Services.AddOptions<AppOptions>()
+            .Bind(appSettingsSection)
+            .ValidateDataAnnotations();
+        var filesXchangeOptions = 
+            appSettingsSection.Get<AppOptions>() ?? new AppOptions();
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.Limits.MaxRequestBodySize = filesXchangeOptions.MaxFileSizeBytes;
+            options.AllowSynchronousIO = true;
+        });
+        builder.Services.Configure<FormOptions>(options =>
+        {
+            options.MultipartBodyLengthLimit = filesXchangeOptions.MaxFileSizeBytes;
+        });
 
-        builder.Services.AddDbContext<FilesXchangeDbContext>(opts 
+        builder.Services.AddDbContext<FilesXchangeDbContext>(opts
             => opts.UseSqlite(builder.Configuration.GetConnectionString("Default")));
         builder.Services.AddControllers();
     }
