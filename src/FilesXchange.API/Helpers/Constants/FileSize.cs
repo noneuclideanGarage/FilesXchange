0f4391f1c8588463e7cdca5781405b485ab4a7bb
Vladislav Martyanov <vladislavmartyanov444@gmail.com>
2026-04-16 14:47:51 +0500

    Add helpful constants in project

---
+++ src/FilesXchange.API/Helpers/Constants/FileSize.cs
@@ -0,0 +1,10 @@
+namespace FilesXchange.API.Helpers.Constants;
+
+
+public static class FileSize
+{
+    public const long KB = 1024L;
+    public const long MB = 1024L * KB;
+    public const long GB = 1024L * MB;
+    public const long TB = 1024L * GB;
+} 
